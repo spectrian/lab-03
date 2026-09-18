20d78954e3b4510d05f497c5c2a6a82f65dc9762
spectrian <felixyang568@gmail.com>
2026-09-18 15:45:09 -0600

    Implement addCity and updateCity methods
    Added methods to add and update cities in the repository.

--- code/app/src/main/java/com/example/listycity3/CityRepository.kt
+++ code/app/src/main/java/com/example/listycity3/CityRepository.kt
@@ -1,7 +1,20 @@
 package com.example.listycity3
 
+import androidx.compose.runtime.mutableStateListOf
+
 class CityRepository {
-    private val _cities = listOf(
+    fun addCity(city: City) {
+        _cities.add(city)
+    }
+
+    fun updateCity(oldCity: City, updatedCity: City) {
+        val index = _cities.indexOf(oldCity)
+        if (index != -1) {
+            _cities[index] = updatedCity
+        }
+    }
+
+    private val _cities = mutableStateListOf(
         City("Edmonton", "AB"),
         City("Vancouver", "BC"),
         City("Toronto", "ON")
